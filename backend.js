97366e3fdada9719ceeb15e69972826702573ebc
eskaigarcia <yosoyeskai@gmail.com>
2025-03-10 10:15:28 +0100

    Push order to timer list

--- backend.js
+++ backend.js
@@ -11,10 +11,55 @@ const data = {
         if(data.currentOrder.mainProduct == null) return
         let order = structuredClone(data.currentOrder)
         data.orderHistory.push(order)
-        data.orderCount++
+        timer.start(data.orderCount, timer.getOrderTimer())
         data.newOrder()
         drawTicket()
         clearSelections()
+        data.orderCount++
+    }
+}
+
+const timer = {
+    activeTimers: [],
+
+    start(order, time){
+        timer.activeTimers.push([order, time])
+
+        let tr = document.createElement('tr')
+        let tdOrder = document.createElement('td')
+        let tdTime = document.createElement('td')
+
+        console.log(order)
+        tdOrder.innerText = String(order + 1).padStart(3, '0')
+
+        mins = Math.floor(time/60)
+        secs = Math.floor(time % 60)
+        tdTime.innerText = mins + ':' + secs
+
+        tr.appendChild(tdOrder)
+        tr.appendChild(tdTime)
+        progressList.appendChild(tr)
+    },
+
+    getOrderTimer() {
+        let fries = data.currentOrder.extras.fries
+        let salad = data.currentOrder.extras.salad
+        let rings = data.currentOrder.extras.rings
+        let extras = data.currentOrder.extraIngredients.length
+    
+        let items = fries + salad + rings + 1
+        let timer = 0
+        for (let i=0; i<items; i++) {
+            timer += Math.random() * 20 + 20
+        }
+        for (let i=0; i<extras; i++){
+            timer += Math.random() * 10 + 5
+        }
+        return timer
+    },
+
+    tick() {
+
     }
 }
 
@@ -49,4 +94,5 @@ const storage = {
 window.onload = function() {
     storage.read()
     data.newOrder()
+    setInterval(timer.tick, 1000)
 };
